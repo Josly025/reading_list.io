fb37bc0c7d315653737d12ab2e80a019a64a5caf
Grantjos12 <grantjoslyn@gmail.com>
2020-12-14 16:15:42 -0600

    new constructor for input entries

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const titleInput = document.getElementById("input-title");
 const authorInput = document.getElementById("input-author");
 const numberInput = document.getElementById("input-number");
 const submitBtn = document.getElementById("submit-btn");
+const tableEntry = document.getElementById("table-entry");
+
+//Entry construcotr for each title, author, and number input
+function Entry(title, author, number) {
+  this.title = title;
+  this.author = author;
+  this.number = number;
+}
 
 submitBtn.addEventListener("click", submit);
 
 function submit(e) {
   e.preventDefault();
-  console.log("clicked");
+  const title = titleInput.value;
+  const author = authorInput.value;
+  const number = numberInput.value;
+  //   console.log(title, author, number);
+
+  const entry = new Entry(title, author, number);
+  console.log(entry);
 }
