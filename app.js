b5215475ace18591624deefc2064c747f1137bb8
Grantjos12 <grantjoslyn@gmail.com>
2020-12-14 17:22:20 -0600

    added addRow and clearInputs functions

--- app.js
+++ app.js
@@ -11,8 +11,32 @@ function Entry(title, author, number) {
   this.number = number;
 }
 
-submitBtn.addEventListener("click", submit);
+// Table constructor
+//Leave empty for prototype methods
+function Table() {}
 
+//newRow (appending new books)
+//Pass in Entry!
+Table.prototype.addRow = function (entry) {
+  const newRow = document.createElement("tr");
+  newRow.setAttribute("class", "table-info");
+  newRow.innerHTML = `
+    <th scope="row">${entry.author}</th>
+    <td>${entry.title}</td>
+    <td>${entry.number}</td>`;
+  tableEntry.appendChild(newRow);
+};
+
+//clearInputs
+// Grab inputs --- Set to empty string to clear fields
+Table.prototype.clearInputs = function () {
+  titleInput.value = "";
+  authorInput.value = "";
+  numberInput.value = "";
+};
+
+//Events and Submit function for input data
+submitBtn.addEventListener("click", submit);
 function submit(e) {
   e.preventDefault();
   const title = titleInput.value;
@@ -22,4 +46,13 @@ function submit(e) {
 
   const entry = new Entry(title, author, number);
   console.log(entry);
+
+  //new Instantiate Table constructor
+  const table = new Table();
+
+  //Add book to list
+  //Pass in Entry object
+  table.addRow(entry);
+  ///Clear input fields (call function) = leave empty
+  table.clearInputs();
 }
